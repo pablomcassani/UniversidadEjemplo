f7ccd7b6ea3f098ce46421647e9e99ab273de2d7
jonathan-veliz <joniveliz1993@gmail.com>
2023-09-19 20:40:53 -0300

    probando
    nombre a las columnas

--- src/Vistas/Inscripciones.java
+++ src/Vistas/Inscripciones.java
@@ -91,13 +91,13 @@ public class Inscripciones extends javax.swing.JInternalFrame {
 
         jTInscripcion.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "ID", "Nombre", "Año"
             }
         ));
         jScrollPane2.setViewportView(jTInscripcion);
